028078a1bd8fc7b39c59b5c1e794a6612b8d2c50
Diego De la Vega <diego.delavega@crehana.com>
2024-10-22 08:41:22 -0500

    Cahnges

--- src/apps/core/static/js/swiper.js
+++ src/apps/core/static/js/swiper.js
@@ -1,4 +1,4 @@
-setTimeout(function() {
+function initializeSwiper() {
     var swiper = new Swiper('.swiper-container.two', {
         pagination: '.swiper-pagination',
         paginationClickable: false,
@@ -6,7 +6,7 @@ setTimeout(function() {
         loop: false,
         centeredSlides: true,
         slidesPerView: 'auto',
-        initialSlide: 2,
+        initialSlide: 1,
         coverflow: {
             rotate: 0,
             stretch: 0,
@@ -16,4 +16,13 @@ setTimeout(function() {
             slideShadows: false,
         }
     });
-}, 1000); // 1 segundo de retraso
+}
+
+// Asegúrate de que las diapositivas se han cargado
+if (document.querySelectorAll('.swiper-slide').length > 0) {
+    initializeSwiper();
+} else {
+    setTimeout(function() {
+        initializeSwiper();
+    }, 1000); // Espera 1 segundo para verificar de nuevo
+}
