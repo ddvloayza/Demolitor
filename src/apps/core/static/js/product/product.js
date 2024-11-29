88714ca3eea5bd539965d2fd4cf661219a123021
Diego De la Vega <diego.delavega@crehana.com>
2024-11-29 09:57:41 -0500

    Changes

--- src/apps/core/static/js/product/product.js
+++ src/apps/core/static/js/product/product.js
@@ -137,7 +137,7 @@ function fetchProducts(categoryUuid = null, sortOrder = null, priceRange = null,
                         
                         <a href="${product.urlDetail}">
                             <img src="${product.imageUrl}" alt="${product.name}" class="w-full h-auto mb-4">
-                            <h3 class="text-black text-center text-xs md:text-lg font-bold overflow-hidden" style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">${product.name}</h3>
+                            <h3 class="text-black text-center text-xs md:text-lg font-bold overflow-hidden h-16" style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">${product.name}</h3>
                             <div class="flex justify-center items-center space-x-1 my-2">
                               <span class="text-yellow-500 text-lg">
                                 ${starsHtml}
